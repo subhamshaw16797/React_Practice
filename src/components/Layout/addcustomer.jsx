e79bbc4671c5e1a833ff0d7566408ff5d2588ae1
subhamshaw16797 <subhamshaw16797@gmail.com>
2021-12-08 18:04:44 +0530

    Added codes

--- src/components/Layout/addcustomer.jsx
+++ src/components/Layout/addcustomer.jsx
@@ -1,41 +1,109 @@
 import React from 'react';
+import axios from 'axios';
+// import { Joi } from 'joi-browser';
 
 class AddCustomer extends React.Component {
     state = {
         customer: {
             username: "",
-            password: "",
-            address: "",
             mobileNumber: "",
             email: "",
             accountNo: "",
-            ifscNo: "",
-            pan: "",
-            bank: {
-                bankName: "",
-                address: "",
-            },
-            cylinder: {
-                type: "",
-                weight: "",
-                strapColor: "",
-                price: "",
-                surrenderCylinder: {
-                    surrenderDate:""
-                },
-            },
-            gasBooking: {
-                localDate: "",
-                status: "",
-                bill:""
-            }
+            pan: ""
         },
-        errorMsg:""
+        errors: {},
+        errorMsg: "",
     };
 
-    
+    // schema = {
+    //     // username: Joi.string()
+    //     //     .pattern(new RegExp('^[a-zA-Z0-9]{3,}$'))
+    //     //     .required(),
+    //     mobileNumber: Joi.string()
+    //         .pattern(new RegExp("^[7-9][0-9]{9}$"))
+    //         .required(),
+    //     email: Joi.string()
+    //         .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    //         .pattern(new RegExp("^[a-zA-Z]{3,}@[a-zA-Z]{2,}.[a-zA-Z]{2,}&"))
+    //         .required(),
+    //     accountNo: Joi.number()
+    //         .integer()
+    //         .required(),
+    //     pan: Joi.string()
+    //         .pattern(new RegExp("^[A-Z]{5}[0-9]{4}[A-Z]{1}$"))
+    //         .required(),
+    // };
+
+    //validate method
+    // validate = () => {
+    //     const errors = {};
+    //     const result = Joi.validate(this.state.customer, this.schema, {
+    //         abortEarly: false
+    //     });
+
+    //     console.log(result);
+
+    //     if (result.error != null)
+    //         for (let item of result.error.details) {
+    //             errors[item.path[0]] = item.message
+    //         }
+    //     return Object.keys(errors).length === 0 ? null : errors;
+    // };
+
+    updateInput = (event) => {
+        this.setState({
+            customer: {
+                ...this.state.customer,
+                [event.target.name]: event.target.value,
+            },
+        });
+        // console.log(this.state.customer);
+        // const customer = { ...this.state.customer };
+        // customer[event.target.name] = event.target.value;
+        // this.setState({
+        //     customer: customer
+        // });
+        // console.log(customer);
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Handle Submit");
+
+        // //validate before sending request
+        // this.setState({
+        //     errors: this.validate()
+        // });
+        // console.log(this.state.errors);
+
+        // if (this.state.errors)
+        //     return;
+
+        // sending request
+        const dataUrl = `http://localhost:8080/customer/addCustomer`;
+        axios
+            .post(dataUrl, this.state.customer)
+            .then((response) => {
+                console.log(response.data);
+                alert(
+                    "Added Customer " +
+                        this.state.customer.username +
+                        " successfully !!!"
+                );
+                this.props.history.push("/customer");
+            })
+            .catch((error) => {
+                this.setState({
+                    ...this.state,
+                    errorMsg: error.response.data.message,
+                });
+            });
+    };
 
     render() {
+        const { customer } = this.state;
+        const { errors, errorMsg } = this.state;
+        console.log(this.state.customer);
         return (
             <section className="landing">
                 <div className="wrapper">
@@ -48,7 +116,18 @@ class AddCustomer extends React.Component {
                                             Add Customer
                                         </h4>
                                     </div>
-                                    <form className="shadow p-3 mt-1 bg-warning rounded text-center">
+                                    {errorMsg && (
+                                        <div
+                                            className="alert alert-danger"
+                                            role="alert"
+                                        >
+                                            {errorMsg}
+                                        </div>
+                                    )}
+                                    <form
+                                        className="shadow p-3 mt-1 bg-warning rounded text-center"
+                                        onSubmit={this.handleSubmit}
+                                    >
                                         <div className="mb-2">
                                             <label
                                                 htmlFor="username"
@@ -62,7 +141,12 @@ class AddCustomer extends React.Component {
                                                 // placeholder="Username"
                                                 id="username"
                                                 name="username"
+                                                value={customer.username}
+                                                onChange={this.updateInput}
                                             />
+                                            {errors && (
+                                                <small>{errors.username}</small>
+                                            )}
                                         </div>
                                         <div className="mb-2">
                                             <label
@@ -77,7 +161,14 @@ class AddCustomer extends React.Component {
                                                 // placeholder="Mobile Number"
                                                 id="mobileNum"
                                                 name="mobileNumber"
+                                                value={customer.mobileNumber}
+                                                onChange={this.updateInput}
                                             />
+                                            {errors && (
+                                                <small>
+                                                    {errors.mobileNumber}
+                                                </small>
+                                            )}
                                         </div>
                                         <div className="mb-2">
                                             <label
@@ -92,11 +183,16 @@ class AddCustomer extends React.Component {
                                                 // placeholder="Email"
                                                 id="email"
                                                 name="email"
+                                                value={customer.email}
+                                                onChange={this.updateInput}
                                             />
+                                            {errors && (
+                                                <small>{errors.email}</small>
+                                            )}
                                         </div>
                                         <div className="mb-2">
                                             <label
-                                                htmlFor="accountNum"
+                                                htmlFor="accountNo"
                                                 className="form-label fw-bold text-black"
                                             >
                                                 Account Number
@@ -105,9 +201,16 @@ class AddCustomer extends React.Component {
                                                 type="text"
                                                 className="form-control"
                                                 // placeholder="Account Number"
-                                                id="accountNum"
-                                                name="accountNumber"
+                                                id="accountNo"
+                                                name="accountNo"
+                                                value={customer.accountNo}
+                                                onChange={this.updateInput}
                                             />
+                                            {errors && (
+                                                <small>
+                                                    {errors.accountNo}
+                                                </small>
+                                            )}
                                         </div>
                                         <div className="mb-2">
                                             <label
@@ -122,7 +225,12 @@ class AddCustomer extends React.Component {
                                                 // placeholder="PAN"
                                                 id="pan"
                                                 name="pan"
+                                                value={customer.pan}
+                                                onChange={this.updateInput}
                                             />
+                                            {errors && (
+                                                <small>{errors.pan}</small>
+                                            )}
                                         </div>
                                         <div className="d-grid gap-2 mt-2">
                                             <button
